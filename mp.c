b6fa6fbc96798e9507eeeb10f8139fc4474b3388
Andrew <119732698+andrewnms@users.noreply.github.com>
2024-06-14 19:23:21 +0800

    added mp

---
+++ mp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void game(char *, int *, float *, float *);
+void print();
+void compute();
+void check(float *, float *);
+
+int main(void)
+{
+    float score, cAnswer, cQuestion;
+    char entry;
+    int i, j;
+    printf("Welcome to the Random Numbers Game!\n[E] Enter the game\n[V] View the Current Score\n[X] Exit the Game\nAnswer: ");
+    scanf(" %c", &entry);
+
+    
+}
